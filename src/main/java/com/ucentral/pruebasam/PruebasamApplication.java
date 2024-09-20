36bdc60d8624a84ba5d2f9704e4101d84470e91f
sam <sambus@gmail.com>
2024-09-19 19:24:54 -0500

    (nueva ; configuracion del proyecto spring)

---
+++ src/main/java/com/ucentral/pruebasam/PruebasamApplication.java
@@ -0,0 +1,13 @@
+package com.ucentral.pruebasam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasamApplication.class, args);
+	}
+
+}
